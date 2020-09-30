529051300eb66a0d0363a15f62a5d9b72c62557d
mildier7923 <mildier@hotmail.com>
2020-09-29 19:37:04 -0500

    Hola Git

---
+++ app/Views/prueba.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<script type=""></script>
+	<title>KTM</title>
+	<style type="text/css">
+		ul{
+			list-style: none;
+			text-align: center;
+		}
+		li{
+			width: 20%;
+			display: inline-block;
+		}
+		header{
+			background:rgba(251,75,4,0.8);
+			padding-top: 1em;
+			padding-bottom: 1em;
+		}
+		img{
+			width: 100%;
+			height: 800px;
+		}
+		main{
+			text-align: center;
+		}
+		footer{
+			text-align: center;
+		    background:rgba(251,75,4,0.8);
+			padding-top: 1em;
+			padding-bottom: 1em;
+		}
+
+	</style>
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>INICIO</li>
+				<li>HISTORIA</li>
+				<li>PRODUCTOS</li>
+				<li>CONTACTO</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<img src="https://nuevodiario.es/fotos/1/ktm-duke-390-2017-6.jpg">
+	</main>
+	<footer>
+		<div class="footer-copyright text-center py-3">© 2020 Copyright:
+		    <a href="https://mdbootstrap.com/"> MILDIER RIOS.COM</a>
+		</div>
+	</footer>	
+</body>
+</html>
